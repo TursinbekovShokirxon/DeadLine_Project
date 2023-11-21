1790d4a623a5833a1e4b51a0ce3ae52697783b90
Javlon <abdurasulov2203@gmail.com>
2023-11-21 13:32:35 +0500

    s

--- Infrastructure/Handlers/ForOrders/OrderCreateHandler.cs
+++ Infrastructure/Handlers/ForOrders/OrderCreateHandler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Handlers.ForOrders
 {
-    internal class OrderCreateHandler
+    public class OrderCreateModel : IRequest<> 
     {
+
+    }
+
+    public class OrderCreateHandler:IRequestHandler<>
+    {
+
     }
 }
