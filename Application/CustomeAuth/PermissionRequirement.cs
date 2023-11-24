96a587755dee4de5459242b53d3398ad532341a5
Javlon <abdurasulov2203@gmail.com>
2023-11-25 00:08:39 +0500

    s

---
+++ Application/CustomeAuth/PermissionRequirement.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.CustomeAuth
+{
+    public class PermissionRequirement:IAuthorizationRequirement
+    {
+        public PermissionRequirement(string permission)
+        {
+                Permission = permission;
+        }
+        public string Permission{ get; set; }
+    }
+}
