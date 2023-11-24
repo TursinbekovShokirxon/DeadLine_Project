fca151b537bd09b82ae17f5c724feb7226f4bde2
Javlon <abdurasulov2203@gmail.com>
2023-11-24 18:57:17 +0500

    s

---
+++ Application/InterfacesModelServices/IPermissionService.cs
@@ -0,0 +1,14 @@
+﻿using Application.InterfacesCrudServices;
+using Domain.Models.Authtification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.InterfacesModelServices
+{
+    public interface IPermissionService:IGetAllService<Permission>
+    {
+    }
+}
