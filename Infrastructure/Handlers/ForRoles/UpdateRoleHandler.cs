6eee63e533539762cffabe0dccd740af43f042fd
Javlon <abdurasulov2203@gmail.com>
2023-11-24 19:45:05 +0500

    s

---
+++ Infrastructure/Handlers/ForRoles/UpdateRoleHandler.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Models.Authtification;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Handlers.ForRoles
+{
+    public class UpdateRoleModel:IRequest<Role>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public virtual ICollection<UserAuth>? UserAuthes { get; set; }
+        public virtual ICollection<Permission>? Permissions { get; set; }
+    }
+    public class UpdateRoleHandler : IRequestHandler<UpdateRoleModel, Role>
+    {
+        public Task<Role> Handle(UpdateRoleModel request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
