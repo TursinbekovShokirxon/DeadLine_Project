f7e13594a7c88ee023c5cb481aade2dd9b6cc3ba
Javlon <abdurasulov2203@gmail.com>
2023-12-03 13:11:29 +0500

    s

---
+++ DeadLineService.UI/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DeadLineService.UI.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult GetUsers()
+        {
+
+            return View();
+        }
+    }
+}
