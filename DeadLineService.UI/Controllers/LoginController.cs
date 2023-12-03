898d3347f00715b3499ec6b8a555b991832249a0
Javlon <abdurasulov2203@gmail.com>
2023-12-03 11:02:18 +0500

    s

--- DeadLineService.UI/Controllers/LoginController.cs
+++ DeadLineService.UI/Controllers/LoginController.cs
@@ -14,26 +14,25 @@ namespace DeadLineService.UI.Controllers
         public async Task<IActionResult> Index([FromForm] UserLoginModel user)
         {
             // peredat API Login https://localhost/api/Auth/Login
+
+            string jsonData = JsonConvert.SerializeObject(user);
+            
             using (HttpClient client = new HttpClient())
             {
                 // Конфигурируем HttpClient по вашим требованиям (например, устанавливаем тайм-ауты, хедеры и т.д.)
-
-
                 // 3. Преобразуйте объект в JSON-строку
-                string jsonData = JsonConvert.SerializeObject(user);
-
                 // 4. Создайте `HttpContent` с использованием `StringContent`
                 using (HttpContent content = new StringContent(jsonData, Encoding.UTF8, "application/json"))
                 {
                     // 5. Выполните POST-запрос
                     HttpResponseMessage response = await client.PostAsync("https://localhost/api/Auth/Login", content);
-
                     // 6. Обработайте ответ, если необходимо
                     if (response.IsSuccessStatusCode)
                     {
                         // Успешно отправлено
                         string responseData = await response.Content.ReadAsStringAsync();
                         await Console.Out.WriteLineAsync(responseData);
+
                         // Ваш код обработки ответа
                     }
                     else
