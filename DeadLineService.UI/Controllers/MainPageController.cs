9f3bebbe43c20f767d582a830765a0cc09ab9cde
Javlon <abdurasulov2203@gmail.com>
2023-12-03 11:39:00 +0500

    s

---
+++ DeadLineService.UI/Controllers/MainPageController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DeadLineService.UI.Controllers
+{
+
+    public class MainPageController : Controller
+    {
+        public IActionResult MainPage()
+        {
+
+            return View();
+        }
+    }
+}
